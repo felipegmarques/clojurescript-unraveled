a76c5bdd69fdcbc941d6c945ec5bad1289fbc12e
Felipe Gonçalves Marques <marques.goncalves.felipe@gmail.com>
2020-07-05 16:13:01 -0300

    First publish

--- playground/mywebapp/build.clj
+++
@@ -1,9 +0,0 @@
-(require '[cljs.build.api :as b])
-
-(b/build "src"
- {:output-to "main.js"
-  :output-dir "out/"
-  :source-map "main.js.map"
-  :main 'myapp.core
-  :verbose true
-  :optimizations :none})
--- watch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-while true; do
-    inotifywait -e close_write,moved_to \
-                --excludei ".git/.*$" -r .
-    make $@;
-done;
